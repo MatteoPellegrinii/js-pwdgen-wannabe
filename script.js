395d8b86edfa44f1695aa39b0dfaf3766d0e3860
MatteoPellegrinii <pellegrini.matteo00@gmail.com>
2022-05-03 15:55:22 +0200

    fine

---
+++ script.js
@@ -0,0 +1,20 @@
+const nome = prompt("What's your name?");
+console.log(nome);
+
+
+const surname = prompt ("What's your last name?")
+console.log(surname);
+
+
+const color = prompt ("What's your favorite color?")
+console.log(color);
+
+const password = nome + surname + color + 21;
+console.log(password);
+
+
+
+document.getElementById("name").innerHTML = `${nome}`;
+document.getElementById("surname").innerHTML = `${surname}`;
+document.getElementById("color").innerHTML = `${color}`;
+document.getElementById("password").innerHTML = `${password}`;
